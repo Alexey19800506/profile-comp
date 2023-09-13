37f20c039c70359e1774ba164605d03212d1baa7
Alexey19800506 <osiyuk19800506@gmail.com>
2023-09-13 13:01:23 +0300

    tas5

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,8 +6,8 @@ const formatDate = date => moment(date).format('DD MMM YYYY');
 const Profile = (props)=> {
     return (
         <div className='profile'>
-        <div className="profile__name">{`${props.user.firstName} ${props.user.lastName}`}</div>
-        <div className="profile__birth">{`Was born ${formatDate(props.user.birthDate)} in ${props.user.birthPlace}`}</div>    
+        <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+        <div className="profile__birth">{`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}</div>    
         </div>
         
     )
